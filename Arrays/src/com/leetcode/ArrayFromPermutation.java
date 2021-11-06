e41875e39d957aad34fdcd4760a0717384a38ae8
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-11-06 23:12:30 +0530

    Intial Commit

---
+++ Arrays/src/com/leetcode/ArrayFromPermutation.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+public class ArrayFromPermutation {
+
+	public static void main(String[] args) {
+		 int[] nums = {0,2,1,5,3,4};
+		buildArray(nums);
+		for(int a : nums)
+		{
+			System.out.println(a);
+		}
+	}
+	public static int[] buildArray(int[] nums) {
+		int[] ans = new int[nums.length];
+		for(int i = 0; i<nums.length; i++){
+			ans[i]=nums[nums[i]];
+		}
+		return ans;  
+	}
+}
