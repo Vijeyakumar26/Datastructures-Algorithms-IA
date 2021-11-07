00688eb225bb995054695883ada3f192f15eba85
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-11-07 17:19:46 +0530

    Intial commit

--- Arrays/src/com/leetcode/ArrayConcatenation.java
+++ Arrays/src/com/leetcode/ArrayConcatenation.java
@@ -1,6 +1,6 @@
-package com.prerequisite.oops;
+package com.leetcode;
 
-public class Zoo {
+public class ArrayConcatenation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
