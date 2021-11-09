77e2985858ba4d53b28d54c6988300952267359b
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-11-09 14:59:51 +0530

    metadata update

--- Arrays/src/com/leetcode/ArrayConcatenation.java
+++ Arrays/src/com/leetcode/ArrayConcatenation.java
@@ -3,8 +3,20 @@ package com.leetcode;
 public class ArrayConcatenation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = {1,2,1};
 
 	}
 
+	public static int[] getConcatenation(int[] nums) {
+		int[] ans = new int[2*nums.length];
+		
+		return nums;
+	}
+
 }
+
+/*
+ * Input: nums = [1,2,1] Output: [1,2,1,1,2,1] Explanation: The array ans is
+ * formed as follows: - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+ * - ans = [1,2,1,1,2,1]
+ */
